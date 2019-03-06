662b21922299d217c6322d9fcbc5f2a83caaa610
Jana <jana.yj.orth@gmail.com>
2019-03-06 15:41:17 +0100

    db sqlite

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type user struct {
-	UserName string
-	Password []byte
-	First    string
-	Last     string
+	UserName string `json:"userName"`
+	Password []byte `json:"password"`
+	First    string `json:"first"`
+	Last     string `json:"last"`
+	ID       int64  `json:"id"`
+}
+
+type Photo struct {
+	ID  int64  `json:"id"`
+	Src string `json:"src"`
+}
+
+type PhotoCollection struct {
+	Photos []Photo `json:"items"`
 }
 
 var tpl *template.Template
@@ -23,22 +36,28 @@ var dbSessions = map[string]string{} // session ID, user ID
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
-	dbUsers["test@test.com"] = user{"test@test.com", bs, "Jana", "O"}
+	dbUsers["test@test.com"] = user{"test@test.com", bs, "Jana", "O", 1234}
 }
 
 func main() {
+
+	// Database
+	db := initialiseDatabase("database/database.sqlite")
+	migrateDatabase(db)
+
+	//Routes
 	http.HandleFunc("/", index)
-	http.HandleFunc("/upload", upload)
+	// http.HandleFunc("/upload", upload)
 	http.HandleFunc("/account", account)
-	http.HandleFunc("/signup", signup)
+	// http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
-
-	http.ListenAndServe(":8080", nil)
+	//using default servemux
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	fmt.Println("dbUsers:", dbUsers)
 }
@@ -57,8 +76,7 @@ func account(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "account.gohtml", u)
 }
 
-// TODO: save in DB ===========!!!
-func signup(w http.ResponseWriter, req *http.Request) {
+func signup(w http.ResponseWriter, req *http.Request, db *sql.DB) {
 	if alreadyLoggedIn(req) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
@@ -83,6 +101,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 			Value: sID.String(),
 		}
 		http.SetCookie(w, c)
+
 		dbSessions[c.Value] = un
 		// store user in dbUsers
 		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
@@ -90,8 +109,34 @@ func signup(w http.ResponseWriter, req *http.Request) {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
-		u = user{un, bs, f, l}
-		dbUsers[un] = u
+		//save user in DB
+		stmt, err := db.Prepare("INSERT INTO users (src) VALUES(?)")
+		if err != nil {
+			panic(err)
+		}
+
+		defer stmt.Close()
+
+		result, err := stmt.Exec(req.FormValue)
+		if err != nil {
+			panic(err)
+		}
+
+		userID, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		user := user{
+			UserName: un,
+			Password: bs,
+			First:    f,
+			Last:     l,
+			ID:       userID,
+		}
+
+		// u = user{un, bs, f, l, id}
+		dbUsers[un] = user
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
 	}
@@ -153,3 +198,55 @@ func logout(w http.ResponseWriter, req *http.Request) {
 
 	http.Redirect(w, req, "/login", http.StatusSeeOther)
 }
+
+// Database
+func initialiseDatabase(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil || db == nil {
+		panic("Error connecting to database")
+	}
+
+	return db
+}
+
+func migrateDatabase(db *sql.DB) {
+	sql := `
+        CREATE TABLE IF NOT EXISTS photos(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                src VARCHAR NOT NULL
+        );
+   `
+	_, err := db.Exec(sql)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// func getPhotos(db *sql.DB) *PhotoCollection {
+
+// 	rows, err := db.Query("SELECT * FROM photos")
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	defer rows.Close()
+
+// 	result := PhotoCollection{}
+
+// 	for rows.Next() {
+// 		photo := Photo{}
+// 		err2 := rows.Scan(&photo.ID, &photo.Src)
+
+// 		if err2 != nil {
+// 			panic(err2)
+// 		}
+
+// 		result.Photos = append(result.Photos, photo)
+// 	}
+
+// 	return *result
+
+// }
--- upload.go
+++ upload.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha1"
+	"database/sql"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,10 +10,12 @@ import (
 	"path/filepath"
 	"strings"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/satori/go.uuid"
 )
 
-func upload(w http.ResponseWriter, req *http.Request) {
+//Upload
+func upload(w http.ResponseWriter, req *http.Request, db *sql.DB) {
 	u := getUser(w, req)
 	if !alreadyLoggedIn(req) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
@@ -23,31 +26,57 @@ func upload(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
 	if req.Method == http.MethodPost {
 		//multipart.File, *multipart.FileHeader, error
-		mf, fh, err := req.FormFile("nf")
+		mf, fh, err := req.FormFile("file")
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		defer mf.Close()
+
 		ext := strings.Split(fh.Filename, ".")[1]
 		// create sha for file name
 		h := sha1.New()
 		io.Copy(h, mf)
 		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
-		// create new file
+
+		// create new file in working directory
 		wd, err := os.Getwd()
 		if err != nil {
 			fmt.Println(err)
 		}
 		path := filepath.Join(wd, "public", "uploads", fname)
-		nf, err := os.Create(path)
+		fileSrc := "http://127.0.0.1:8080/uploads/" + fh.Filename
+
+		file, err := os.Create(path)
 		if err != nil {
 			fmt.Println(err)
 		}
-		defer nf.Close()
-		// copy
+		//saving into db
+		stmt, err := db.Prepare("INSERT INTO photos (src) VALUES(?)")
+		if err != nil {
+			panic(err)
+		}
+
+		defer stmt.Close()
+		result, err := stmt.Exec(fileSrc)
+		if err != nil {
+			panic(err)
+		}
+		//id from db package
+		photoID, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		photo := Photo{
+			ID:  photoID,
+			Src: fileSrc,
+		}
+
+		defer file.Close()
+
 		mf.Seek(0, 0)
-		io.Copy(nf, mf)
-		// add filename to this user's cookie
+		io.Copy(file, mf)
 		c = appendValue(w, c, fname)
 	}
 	xs := strings.Split(c.Value, "|")
@@ -68,6 +97,7 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 	return c
 }
 
+//append picture filename to cookie value
 func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
 	s := c.Value
 	if !strings.Contains(s, fname) {
